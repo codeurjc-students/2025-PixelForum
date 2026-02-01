4eb119612aadeacd91e89648dbe251893d60337b
martingutpar <m.gutierrezp.2022@alumnos.urjc.es>
2026-02-01 17:27:33 +0100

    Add homepage html + add header and sidebar components

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { PostListComponent } from './components/post-list/post-list.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [PostListComponent],
+  imports: [PostListComponent, HeaderComponent, SidebarComponent],
 })
 export class App {
 }
